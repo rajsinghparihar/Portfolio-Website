11ebbfb43b7c1380153eb356fc006d15e7bf8f37
rajsinghparihar <rajsingh212001@gmail.com>
2021-07-11 04:31:31 +0530

    changed theme

--- src/mock/data.js
+++ src/mock/data.js
@@ -41,7 +41,7 @@ export const projectsData = [
     title: 'PsypherX: CLI App for Password Encryption',
     info: 'Secure your passwords with AES encryption with this cool terminal text animations based app',
     info2: 'made using pycryptodome,pyinquirer, and python, the motive of this project was to learn how to use complex libraries in python and integrate them in a working app',
-    url: '',
+    url: '/404',
     repo: 'https://github.com/rajsinghparihar/PsypherX', // if no repo, the button will not show up
   },
 ];
