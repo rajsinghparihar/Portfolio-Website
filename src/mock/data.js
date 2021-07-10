afb589506a88fc50f9e3377239b042558b3718b1
Raj Singh Parihar <rajsingh212001@gmail.com>
2021-07-11 04:01:38 +0530

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -21,7 +21,7 @@ export const aboutData = {
   paragraphOne: "I'm a student at IIIT-H, I code when I'm not gaming.",
   paragraphTwo: "I like game development in unity, video editing and content creation for youtube. I've been spending most of my time trying to learn new skills recently, my most recently learned skill is web scraping using Beautiful soup library from python.",
   paragraphThree: 'Currently working on a IoT based project for Ease of Lab Experiments performed virtually : Remote Triggered Lab as my honors research project',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://drive.google.com/file/d/1q1XSdKAMbhXedLv1JMegZubqwwWcQ5lI/view?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -34,6 +34,15 @@ export const projectsData = [
     info2: 'made using pygame, the motive of this project was to learn how to use various libraries in python and integrate them in a working app/game',
     url: 'https://www.youtube.com/watch?v=xi-AxAlxCAo',
     repo: 'https://github.com/rajsinghparihar/PyGame', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: '',
+    title: 'PsypherX: CLI App for Password Encryption',
+    info: 'Secure your passwords with AES encryption with this cool terminal text animations based app',
+    info2: 'made using pycryptodome,pyinquirer, and python, the motive of this project was to learn how to use complex libraries in python and integrate them in a working app',
+    url: '',
+    repo: 'https://github.com/rajsinghparihar/PsypherX', // if no repo, the button will not show up
   },
 ];
 
